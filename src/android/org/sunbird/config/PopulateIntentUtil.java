65ff7382409e05ad924c3d207d58ca3290c01aad
subranil <subranil.saha@tarento.com>
2021-03-18 14:58:20 +0530

    Issue #SB-23189 CHORE: refactored code

---
+++ src/android/org/sunbird/config/PopulateIntentUtil.java
@@ -0,0 +1,164 @@
+package org.sunbird.config;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class PopulateIntentUtil {
+
+    public static Intent populateIntent(JSONObject obj, CallbackContext callbackContext) throws JSONException {
+        String type = obj.has("type") ? obj.getString("type") : null;
+        String packageAssociated = obj.has("package") ? obj.getString("package") : null;
+
+        Uri uri = null;
+        if (obj.has("url")) {
+            String uriAsString = obj.getString("url");
+        }
+
+        JSONObject extras = obj.has("extras") ? obj.getJSONObject("extras") : null;
+        Map<String, Object> extrasMap = new HashMap<String, Object>();
+        Bundle bundle = null;
+        String bundleKey = "";
+        if (extras != null) {
+            JSONArray extraNames = extras.names();
+            for (int i = 0; i < extraNames.length(); i++) {
+                String key = extraNames.getString(i);
+                Object extrasObj = extras.get(key);
+                if (extrasObj instanceof JSONObject) {
+                    //  The extra is a bundle
+                    bundleKey = key;
+                    bundle = toBundle((JSONObject) extras.get(key));
+                } else {
+                    extrasMap.put(key, extras.get(key));
+                }
+            }
+        }
+
+        String action = obj.has("action") ? obj.getString("action") : null;
+        Intent i = new Intent();
+        if (action != null)
+            i.setAction(action);
+
+        if (type != null && uri != null) {
+            i.setDataAndType(uri, type); //Fix the crash problem with android 2.3.6
+        } else {
+            if (type != null) {
+                i.setType(type);
+            }
+            if (uri != null) {
+                i.setData(uri);
+            }
+        }
+
+        JSONObject component = obj.has("component") ? obj.getJSONObject("component") : null;
+        if (component != null) {
+            //  User has specified an explicit intent
+            String componentPackage = component.has("package") ? component.getString("package") : null;
+            String componentClass = component.has("class") ? component.getString("class") : null;
+            if (componentPackage == null || componentClass == null) {
+                Log.w("UtilityClassPlugin", "Component specified but missing corresponding package or class");
+                throw new JSONException("Component specified but missing corresponding package or class");
+            } else {
+                ComponentName componentName = new ComponentName(componentPackage, componentClass);
+                i.setComponent(componentName);
+            }
+        }
+
+        if (packageAssociated != null)
+            i.setPackage(packageAssociated);
+
+
+
+        if (bundle != null)
+            i.putExtra(bundleKey, bundle);
+
+        for (String key : extrasMap.keySet()) {
+            Object value = extrasMap.get(key);
+            String valueStr = String.valueOf(value);
+
+            if (value instanceof Boolean) {
+                i.putExtra(key, Boolean.valueOf(valueStr));
+            } else if (value instanceof Integer) {
+                i.putExtra(key, Integer.valueOf(valueStr));
+            } else if (value instanceof Long) {
+                i.putExtra(key, Long.valueOf(valueStr));
+            } else if (value instanceof Double) {
+                i.putExtra(key, Double.valueOf(valueStr));
+            } else {
+                i.putExtra(key, valueStr);
+            }
+
+        }
+        return i;
+    }
+
+
+    private static Bundle toBundle(final JSONObject obj) {
+        Bundle returnBundle = new Bundle();
+        if (obj == null) {
+            return null;
+        }
+        try {
+            Iterator<?> keys = obj.keys();
+            while (keys.hasNext()) {
+                String key = (String) keys.next();
+                Object compare = obj.get(key);
+                if (obj.get(key) instanceof String)
+                    returnBundle.putString(key, obj.getString(key));
+                else if (obj.get(key) instanceof Boolean)
+                    returnBundle.putBoolean(key, obj.getBoolean(key));
+                else if (obj.get(key) instanceof Integer)
+                    returnBundle.putInt(key, obj.getInt(key));
+                else if (obj.get(key) instanceof Long)
+                    returnBundle.putLong(key, obj.getLong(key));
+                else if (obj.get(key) instanceof Double)
+                    returnBundle.putDouble(key, obj.getDouble(key));
+                else if (obj.get(key).getClass().isArray() || obj.get(key) instanceof JSONArray) {
+                    JSONArray jsonArray = obj.getJSONArray(key);
+                    int length = jsonArray.length();
+                    if (jsonArray.get(0) instanceof String) {
+                        String[] stringArray = new String[length];
+                        for (int j = 0; j < length; j++)
+                            stringArray[j] = jsonArray.getString(j);
+                        returnBundle.putStringArray(key, stringArray);
+                        //returnBundle.putParcelableArray(key, obj.get);
+                    } else {
+                        if (key.equals("PLUGIN_CONFIG")) {
+                            ArrayList<Bundle> bundleArray = new ArrayList<Bundle>();
+                            for (int k = 0; k < length; k++) {
+                                bundleArray.add(toBundle(jsonArray.getJSONObject(k)));
+                            }
+                            returnBundle.putParcelableArrayList(key, bundleArray);
+                        } else {
+                            Bundle[] bundleArray = new Bundle[length];
+                            for (int k = 0; k < length; k++)
+                                bundleArray[k] = toBundle(jsonArray.getJSONObject(k));
+                            returnBundle.putParcelableArray(key, bundleArray);
+                        }
+                    }
+                } else if (obj.get(key) instanceof JSONObject)
+                    returnBundle.putBundle(key, toBundle((JSONObject) obj.get(key)));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        return returnBundle;
+    }
+
+
+}
--- src/android/org/sunbird/config/UtilityPlugin.java
+++ src/android/org/sunbird/config/UtilityPlugin.java
@@ -1,10 +1,6 @@
 package org.sunbird.config;
 
-import android.Manifest;
 import android.content.ActivityNotFoundException;
-import android.content.ClipData;
-import android.content.ComponentName;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -12,17 +8,9 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
-import android.os.Bundle;
 import android.os.Environment;
-import android.text.Html;
 import android.text.TextUtils;
 import androidx.annotation.NonNull;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.webkit.MimeTypeMap;
-
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.CommonStatusCodes;
@@ -34,28 +22,23 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaResourceApi;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.sunbird.storage.StorageUtil;
 import org.sunbird.utm.InstallReferrerListener;
 import org.sunbird.utm.PlayStoreInstallReferrer;
-import org.sunbird.storage.StorageUtil;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
-import io.github.pwlin.cordova.plugins.fileopener2.FileProvider;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -157,7 +140,7 @@ public class UtilityPlugin extends CordovaPlugin {
                 return false;
             }
             JSONObject object = args.getJSONObject(0);
-            Intent intent = populateIntent(object, callbackContext);
+            Intent intent = PopulateIntentUtil.populateIntent(object, callbackContext);
             int requestCode = object.has("requestCode") ? object.getInt("requestCode") : 1;
             this.onActivityResultCallbackContext = callbackContext;
             startActivity(intent, requestCode, callbackContext);
@@ -625,147 +608,6 @@ public class UtilityPlugin extends CordovaPlugin {
         callbackContext.success(availableAppsMap);
     }
 
-    private Intent populateIntent(JSONObject obj, CallbackContext callbackContext) throws JSONException {
-        String type = obj.has("type") ? obj.getString("type") : null;
-        String packageAssociated = obj.has("package") ? obj.getString("package") : null;
-
-        Uri uri = null;
-        final CordovaResourceApi resourceApi = webView.getResourceApi();
-        if (obj.has("url")) {
-            String uriAsString = obj.getString("url");
-        }
-
-        JSONObject extras = obj.has("extras") ? obj.getJSONObject("extras") : null;
-        Map<String, Object> extrasMap = new HashMap<String, Object>();
-        Bundle bundle = null;
-        String bundleKey = "";
-        if (extras != null) {
-            JSONArray extraNames = extras.names();
-            for (int i = 0; i < extraNames.length(); i++) {
-                String key = extraNames.getString(i);
-                Object extrasObj = extras.get(key);
-                if (extrasObj instanceof JSONObject) {
-                    //  The extra is a bundle
-                    bundleKey = key;
-                    bundle = toBundle((JSONObject) extras.get(key));
-                } else {
-                    extrasMap.put(key, extras.get(key));
-                }
-            }
-        }
-
-        String action = obj.has("action") ? obj.getString("action") : null;
-        Intent i = new Intent();
-        if (action != null)
-            i.setAction(action);
-
-        if (type != null && uri != null) {
-            i.setDataAndType(uri, type); //Fix the crash problem with android 2.3.6
-        } else {
-            if (type != null) {
-                i.setType(type);
-            }
-            if (uri != null) {
-                i.setData(uri);
-            }
-        }
-
-        JSONObject component = obj.has("component") ? obj.getJSONObject("component") : null;
-        if (component != null) {
-            //  User has specified an explicit intent
-            String componentPackage = component.has("package") ? component.getString("package") : null;
-            String componentClass = component.has("class") ? component.getString("class") : null;
-            if (componentPackage == null || componentClass == null) {
-                Log.w("UtilityClassPlugin", "Component specified but missing corresponding package or class");
-                throw new JSONException("Component specified but missing corresponding package or class");
-            } else {
-                ComponentName componentName = new ComponentName(componentPackage, componentClass);
-                i.setComponent(componentName);
-            }
-        }
-
-        if (packageAssociated != null)
-            i.setPackage(packageAssociated);
-
-
-
-        if (bundle != null)
-            i.putExtra(bundleKey, bundle);
-
-        for (String key : extrasMap.keySet()) {
-            Object value = extrasMap.get(key);
-            String valueStr = String.valueOf(value);
-
-            if (value instanceof Boolean) {
-                i.putExtra(key, Boolean.valueOf(valueStr));
-            } else if (value instanceof Integer) {
-                i.putExtra(key, Integer.valueOf(valueStr));
-            } else if (value instanceof Long) {
-                i.putExtra(key, Long.valueOf(valueStr));
-            } else if (value instanceof Double) {
-                i.putExtra(key, Double.valueOf(valueStr));
-            } else {
-                i.putExtra(key, valueStr);
-            }
-
-        }
-        return i;
-    }
-
-    private Bundle toBundle(final JSONObject obj) {
-        Bundle returnBundle = new Bundle();
-        if (obj == null) {
-            return null;
-        }
-        try {
-            Iterator<?> keys = obj.keys();
-            while (keys.hasNext()) {
-                String key = (String) keys.next();
-                Object compare = obj.get(key);
-                if (obj.get(key) instanceof String)
-                    returnBundle.putString(key, obj.getString(key));
-                else if (obj.get(key) instanceof Boolean)
-                    returnBundle.putBoolean(key, obj.getBoolean(key));
-                else if (obj.get(key) instanceof Integer)
-                    returnBundle.putInt(key, obj.getInt(key));
-                else if (obj.get(key) instanceof Long)
-                    returnBundle.putLong(key, obj.getLong(key));
-                else if (obj.get(key) instanceof Double)
-                    returnBundle.putDouble(key, obj.getDouble(key));
-                else if (obj.get(key).getClass().isArray() || obj.get(key) instanceof JSONArray) {
-                    JSONArray jsonArray = obj.getJSONArray(key);
-                    int length = jsonArray.length();
-                    if (jsonArray.get(0) instanceof String) {
-                        String[] stringArray = new String[length];
-                        for (int j = 0; j < length; j++)
-                            stringArray[j] = jsonArray.getString(j);
-                        returnBundle.putStringArray(key, stringArray);
-                        //returnBundle.putParcelableArray(key, obj.get);
-                    } else {
-                        if (key.equals("PLUGIN_CONFIG")) {
-                            ArrayList<Bundle> bundleArray = new ArrayList<Bundle>();
-                            for (int k = 0; k < length; k++) {
-                                bundleArray.add(toBundle(jsonArray.getJSONObject(k)));
-                            }
-                            returnBundle.putParcelableArrayList(key, bundleArray);
-                        } else {
-                            Bundle[] bundleArray = new Bundle[length];
-                            for (int k = 0; k < length; k++)
-                                bundleArray[k] = toBundle(jsonArray.getJSONObject(k));
-                            returnBundle.putParcelableArray(key, bundleArray);
-                        }
-                    }
-                } else if (obj.get(key) instanceof JSONObject)
-                    returnBundle.putBundle(key, toBundle((JSONObject) obj.get(key)));
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-
-        return returnBundle;
-    }
-
     private void startActivity(Intent intent, int requestCode, CallbackContext callbackContext) {
         if (intent.resolveActivityInfo(this.cordova.getActivity().getPackageManager(), 0) != null) {
             cordova.setActivityResultCallback(this);
@@ -779,87 +621,19 @@ public class UtilityPlugin extends CordovaPlugin {
         if (onActivityResultCallbackContext != null && intent != null) {
             intent.putExtra("requestCode", requestCode);
             intent.putExtra("resultCode", resultCode);
-            PluginResult result = new PluginResult(PluginResult.Status.OK, getIntentJson(intent));
+            PluginResult result = new PluginResult(PluginResult.Status.OK, GetIntentJsonUtil.getIntentJson(intent));
             result.setKeepCallback(true);
             onActivityResultCallbackContext.sendPluginResult(result);
         } else if (onActivityResultCallbackContext != null) {
             Intent canceledIntent = new Intent();
             canceledIntent.putExtra("requestCode", requestCode);
             canceledIntent.putExtra("resultCode", resultCode);
-            PluginResult canceledResult = new PluginResult(PluginResult.Status.OK, getIntentJson(canceledIntent));
+            PluginResult canceledResult = new PluginResult(PluginResult.Status.OK, GetIntentJsonUtil.getIntentJson(canceledIntent));
             canceledResult.setKeepCallback(true);
             onActivityResultCallbackContext.sendPluginResult(canceledResult);
         }
 
         this.onActivityResultCallbackContext = null;
     }
-
-    private JSONObject getIntentJson(Intent intent) {
-        JSONObject intentJSON = null;
-        try {
-            intentJSON = new JSONObject();
-            intentJSON.put("type", intent.getType());
-            intentJSON.put("extras", toJsonObject(intent.getExtras()));
-            intentJSON.put("action", intent.getAction());
-            intentJSON.put("categories", intent.getCategories());
-            intentJSON.put("flags", intent.getFlags());
-            intentJSON.put("component", intent.getComponent());
-            intentJSON.put("data", intent.getData());
-            intentJSON.put("package", intent.getPackage());
-            return intentJSON;
-        } catch (JSONException e) {
-            Log.d("UtilityPlugin", " Error thrown during intent > JSON conversion");
-            Log.d("UtilityPlugin", e.getMessage());
-            Log.d("UtilityPlugin", Arrays.toString(e.getStackTrace()));
-            return null;
-        }
-    }
-
-    private static JSONObject toJsonObject(Bundle bundle) {
-        //  Credit: https://github.com/napolitano/cordova-plugin-intent
-        try {
-            return (JSONObject) toJsonValue(bundle);
-        } catch (JSONException e) {
-            throw new IllegalArgumentException("Cannot convert bundle to JSON: " + e.getMessage(), e);
-        }
-    }
-
-    private static Object toJsonValue(final Object value) throws JSONException {
-        //  Credit: https://github.com/napolitano/cordova-plugin-intent
-        if (value == null) {
-            return null;
-        } else if (value instanceof Bundle) {
-            final Bundle bundle = (Bundle) value;
-            final JSONObject result = new JSONObject();
-            for (final String key : bundle.keySet()) {
-                result.put(key, toJsonValue(bundle.get(key)));
-            }
-            return result;
-        } else if ((value.getClass().isArray())) {
-            final JSONArray result = new JSONArray();
-            int length = Array.getLength(value);
-            for (int i = 0; i < length; ++i) {
-                result.put(i, toJsonValue(Array.get(value, i)));
-            }
-            return result;
-        } else if (value instanceof ArrayList<?>) {
-            final ArrayList arrayList = (ArrayList<?>) value;
-            final JSONArray result = new JSONArray();
-            for (int i = 0; i < arrayList.size(); i++)
-                result.put(toJsonValue(arrayList.get(i)));
-            return result;
-        } else if (
-                value instanceof String
-                        || value instanceof Boolean
-                        || value instanceof Integer
-                        || value instanceof Long
-                        || value instanceof Double) {
-            return value;
-        } else {
-            return String.valueOf(value);
-        }
-    }
-
-
 
 }
